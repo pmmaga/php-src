ac225c5ab21abe1197f9d3318932363ba76c78cd
Pedro Magalhães <pmmaga@php.net>
2019-02-06 19:10:57 +0000

    full matrix

--- travis/os/osx.sh
+++ travis/os/osx.sh
@@ -1,8 +1,8 @@
 # Set expected paths
 export PATH="/usr/local/opt/bison/bin:/usr/local/opt/ccache/libexec:$PATH"
-export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/libedit/lib/pkgconfig:/usr/local/opt/zlib/lib/pkgconfig"
-export CFLAGS="-Wno-nullability-completeness -Wno-expansion-to-defined"
-export CXXFLAGS="-Wno-nullability-completeness -Wno-expansion-to-defined"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/libedit/lib/pkgconfig:/usr/local/opt/zlib/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CFLAGS="-Wno-nullability-completeness -Wno-expansion-to-defined $CFLAGS"
+export CXXFLAGS="-Wno-nullability-completeness -Wno-expansion-to-defined $CXXFLAGS"
 # Force relinking
 brew link icu4c gettext --force
 # Start DB services
