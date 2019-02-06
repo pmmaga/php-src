6eeb79ccc3ebf55df36f98f4bc506aabcb126601
Pedro Magalhães <pmmaga@php.net>
2019-02-06 17:16:44 +0000

    Split OS specific setup to own files

---
+++ travis/os/osx.sh
@@ -0,0 +1,9 @@
+# Set expected paths
+export PATH="/usr/local/opt/bison/bin:/usr/local/opt/ccache/libexec:$PATH"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/libedit/lib/pkgconfig:/usr/local/opt/zlib/lib/pkgconfig"
+export CFLAGS="-Wno-nullability-completeness"
+# Force relinking
+brew link icu4c gettext --force
+# Start DB services
+brew services start mysql
+brew services start postgresql
